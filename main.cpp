f3e74a3377acf1a25359387f95d9d3e8d4cf6c1d
toqakhalid <44583119+toqakhalid@users.noreply.github.com>
2018-11-01 19:16:24 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,59 +1,94 @@
 #include <iostream>
-#include "matrix.h"
-
-
+#include "BigInt.h"
 using namespace std;
 
-
-
-
+//Developed and Coded by YUSUF FAWZY ELNADY & NERMINE MOHSEN ARAFA
+ostream& operator<<(ostream& out, const BigInt& in )
+{
+    out<<in.decStr;
+    return out;
+}
+istream& operator>>(istream& in, BigInt& number)
+{
+    in>>number.decStr ;
+    return in ;
+}
 
 int main()
 {
-    int data1 [] = {1,2,3,4,5,6,7,8};
-    int data2 [] = {13,233,3,4,5,6};
-    int data3 [] = {10,100,10,100,10,100,10,100};
-    int data4 [] = {1,0,0,0,1,0,0,0,1};
 
+    int choice;
+    string infile,outfile;
+    cout<<"1 for input, 2 for file";
+    string in1,in2;
+    cin>>choice;
+    if (choice==1)
+    {
+        cout<<"Enter the First Number\n";
+        cin>>in1;
+        cout<<"Enter the Second Number\n";
+        cin>>in2;
+        BigInt n1(in1);
+        BigInt n2(in2);
+        BigInt n4=n1-n2,
+        n3=n2+n1;
 
-    matrix mat1 (4, 2, data1, mat1);
-    matrix mat2 (2, 3, data2, mat2);
-    matrix mat3(4, 2, data3, mat3);
-    matrix mat4 (3,3,data4,mat4);
+        cout<<n1<<"-"<<n2<<"="<<n4<<endl;
+        cout<<n2<<"+"<<n1<<"="<<n3<<endl;
+      //  cout<<"POSTFIX: n4++: "<<n4++<<endl<<"POSTFIX: n4--"<<n4--<<endl<<"n4 now is "<<n4<<endl;
+        cout<<endl<<endl;;
+      /*  if (n1<n2)
+        {
+            cout<<"n1 < n2"<<endl;
+        }
+        if (n1!=n2)
+        {
+            cout<<"Not Equal"<<endl;
+        }
+        if (n1>n2)
+        {
+            cout<<"n1 > n2"<<endl;
+        }
 
-    cout<< "Matrix 1 is :"<< endl;
-    cout << mat1 << endl;
-    cout<< "Matrix 2 is :"<< endl;
-    cout << mat2 << endl;
-   // cout<< "Matrix 3 is :"<< endl;
-   // cout << mat3 << endl;
-  //  cout<< "Matrix 4 is :"<< endl;
-   // cout<< mat4<< endl;
-///cout<< "The addition of two matrices 3 and 3 is : " << endl<< (mat1 += mat3) << endl;
-     /// cout<< "opp-= is : " << endl<< (mat1 -= mat3) << endl;
+        if (n1==n2)
+        {
+            cout<<"EQUAL"<<endl;
+        }*/
+    }
+    else if (choice ==2)
+    {
+        cout<<"Enter the name for your output file such as \"name.txt\"";
+        cin>>outfile;
+        ifstream filein ("numbers.txt",ios::in);
+        ofstream fileout (outfile.c_str(),ios::out);
+        filein>>in1>>in2;
+        BigInt n1(in1);
+        BigInt n2(in2);
+        BigInt n4=n2-n1,n3=n2+n1;
+        fileout<<n1<<"-"<<n2<<"="<<n4<<endl;
+        fileout<<n2<<"+"<<n1<<"="<<n3<<endl;
+        fileout<<"POSTFIX: n4++: "<<n4++<<endl;
+               fileout<<"POSTFIX: n4--: "<<n4++<<endl<<"NEW n4: "<<n4<<endl;
+        if (n1<n2)
+        {
+            fileout<<"n1 < n2"<<endl;
+        }
+        if (n1!=n2)
+        {
+            fileout<<"Not Equal"<<endl;
+        }
+        if (n1>n2)
+        {
+            fileout<<"n1 > n2"<<endl;
+        }
 
- ///cout <<" mat1-- is =  " <<endl<<(mat1--)<<endl;
-/// cout <<" mat1++ is =  " <<endl<<(mat1++)<<endl;
-/// cout << (mat2+=1)<<endl;
-///cout << (mat2-=1)<<endl;
-
-
-
-  /// cout << "The addition of two matrices 1 and 3 is :" << endl<< (mat1 + mat3) << endl;
-  ///cout << "The addition of two matrices 3 and 3 is :" << endl<< (mat3 + mat3) << endl;
-  /// cout << "The differance of two matrices is :"        << endl<< (mat3 - mat1) << endl;
-  /// cout<< "Addition scalar is :"       << endl << mat1.operator+(2) << endl;
-  ///cout<< "Diffirance scalar is :"     << endl << mat1.operator-(1) << endl;
-  ///cout<< "Multiplication scalar is :" << endl << mat1.operator*(2) << endl;
-
-
-
-  ///cout << (mat1 == mat3)<< endl;
-///cout<< (mat1!= mat3)<<endl ;
-//cout << isSymetric(mat4)<<endl;
-// cout<< "identity = "<< endl << isIdentity(mat4) <<endl;
-//cout<< "squ = "<<endl<< isSquare (mat1)<<endl;
-//cout<< "trasnpose =  "<<endl<< transpose(mat5) ;
+        if (n1==n2)
+        {
+            fileout<<"EQUAL"<<endl;
+        }
+        filein.close();
+        fileout.close();
+    }
 
 
     return 0;
